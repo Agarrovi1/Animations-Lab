1a85fd12d87cb71fa21b6f41966de52d9bdb6583
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-11 14:23:56 -0400

    initial commit

---
+++ Animation-Lab/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Animation-Lab
+//
+//  Created by Angela Garrovillas on 10/11/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var circle: UIView = {
+        var view = UIView()
+        var frame = view.frame
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
