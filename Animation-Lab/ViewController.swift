0384997d9f5f2b00b9a6caaf7f3d41f10ffd3dec
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-11 14:33:15 -0400

    adds circle in middle of view

--- Animation-Lab/ViewController.swift
+++ Animation-Lab/ViewController.swift
@@ -10,14 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var circle: UIView = {
+    lazy var circle: UIView = {
         var view = UIView()
         var frame = view.frame
+        frame.size.height = 150
+        frame.size.width = 150
+        view.frame = frame
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        view.layer.borderColor = UIColor.yellow.cgColor
+        view.layer.borderWidth = 2
+        view.layer.cornerRadius = view.frame.height / 2
         return view
     }()
-
+    
+    private func addSubviews() {
+        self.view.addSubview(circle)
+    }
+    private func setConstraints() {
+        circle.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+        circle.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        circle.heightAnchor.constraint(equalToConstant: circle.frame.height).isActive = true
+        circle.widthAnchor.constraint(equalToConstant: circle.frame.width).isActive = true
+    }
+    private func setUI() {
+        self.view.backgroundColor = .white
+        addSubviews()
+        setConstraints()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
         // Do any additional setup after loading the view.
     }
 
